4346deaf86814e41aecc4dae4361516b300c2fcd
ArdianOSM <realardosmani@gmail.com>
2023-10-26 14:21:13 +0200

    FinalArWebView

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, View, StyleSheet, Modal, Text, Linking } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const WEB_PAGE_URL = 'https://www.arvision.biz/ar'; // Replace with your desired URL
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+
+  const openLinkInBrowserHandler = () => {
+    Linking.canOpenURL(WEB_PAGE_URL).then((supported) =>{
+      supported && Linking.openURL(WEB_PAGE_URL);
+    })
+  }
+
+  const openLinkInWebView = () => setVisible(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+      title={'Open Link in Browser'}
+      onPress={openLinkInBrowserHandler}
+      />
+      <Button title="Open Link in Browser" onPress={openLinkInWebView} />
+
+      <Modal
+        visible={visible}
+        presentationStyle={'pageSheet'}
+        animationType={'slide'}
+        onRequestClose={() => setVisible(false)}
+      >
+        <View  source={{ uri: WEB_PAGE_URL }} />
+        <Button title="Close" onPress={() => setVisible(false)} />
+      </Modal>
     </View>
   );
 }
@@ -17,4 +43,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  seperator: {
+    height: 20,
+  }
 });
